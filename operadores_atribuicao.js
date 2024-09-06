bce4f53ae4e0c9c484eb5fd5d8b486ca79c999bc
Roberta Rie Flushio Ohata <160445345+RobertaFlushio@users.noreply.github.com>
2024-09-06 07:06:12 -0300

    Add files via upload

---
+++ operadores_atribuicao.js
@@ -0,0 +1,9 @@
+let texto = 'listas de aprovados\n';
+texto += '===================\n';
+texto += '1. Robeta Flushio\n';
+texto += '2. Cesar Chiodi\n';
+texto += '3. Andre Luiz\n';
+texto += '4. Marcelo torres\n';
+texto += '5. joãozinho';
+
+console.log(texto);
