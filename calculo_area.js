d9b86f848cda05ceb31f4957745b1551d2b82c14
Roberta Rie Flushio Ohata <160445345+RobertaFlushio@users.noreply.github.com>
2024-09-05 22:52:31 -0300

    Comitando conteúdo inicial
    Algoritmos e lógica.

---
+++ calculo_area.js
@@ -0,0 +1,6 @@
+const pi = 3.141592;
+let raio = 10;
+
+let area = pi * raio * raio;
+
+console.log("o valor da area e: " + area +"m2. ");
---
+++ operadores_relacionaris.js
@@ -0,0 +1,16 @@
+let a = 1;
+let b = 2
+
+console.log('valores true:');
+console.log(a < b);
+console.log(a <= b);
+console.log(a != b);
+console.log(a !== b);
+console.log(a +a === b);
+
+console.log('valores false:');
+console.log(a >b);
+console.log(a >= b);
+console.log(a == b);
+console.log(a === b);
+console.log(a +  a !== b);
