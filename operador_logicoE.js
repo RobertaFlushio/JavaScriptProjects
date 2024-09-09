4745e1d255da3a942ab9da77a46c20e24bf93c22
Roberta Rie Flushio Ohata <160445345+RobertaFlushio@users.noreply.github.com>
2024-09-08 22:27:55 -0300

    Add files via upload

---
+++ operador_logicoE.js
@@ -0,0 +1,9 @@
+let domingo = true
+let noveHoras = true
+
+console.log("Hoje posso dormir mais um pouco?", domingo && noveHoras);
+
+domingo = false
+noveHoras = false
+
+console.log("Hoje tenho que ir para faculdade?", domingo && noveHoras);
