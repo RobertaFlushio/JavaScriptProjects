6c78827ef858ed0f7fd492fcc892fbf6c4cabd03
Roberta Rie Flushio Ohata <160445345+RobertaFlushio@users.noreply.github.com>
2024-09-08 22:51:52 -0300

    Add files via upload

---
+++ operador_logico_aritmedico.js
@@ -0,0 +1,12 @@
+let verdadero1 = 2 + 5 >= 7 && 8 > 2;
+let verdadero2 = !(6 * 6 < 3 ); 
+
+console.log(verdadero1);
+console.log(verdadero2);
+
+
+let falso1 = 8 - 4 > 12 || false || 15 === "15";
+let falso2 = 11 + 11 == 22 && "a" == "b";
+
+console.log(falso1);
+console.log(falso2);
